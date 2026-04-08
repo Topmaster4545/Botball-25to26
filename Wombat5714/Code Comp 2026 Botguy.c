4a23b013a78d3e5826d004bad497890d89627af8
Dylan H. <topmaster4545@gmail.com>
2026-04-07 21:44:05 -0400

    4/7/26 Code Changes
    - Made small tweaks to fix bugs in the overall run
    - Perfected accumulation of items in front of the ramp so they are ready to get plowed up
    - Made code to drop the plow and bring the robot up the ramp (wip)
    - Done by Tanvir

--- Wombat5714/Code Comp 2026 Botguy.c
+++ Wombat5714/Code Comp 2026 Botguy.c
@@ -69,7 +69,7 @@ void setClawPosition(int pos)
 // SENSORS --------------------------------------------------------
 
 struct Sensor {
-	int port;
+    int port;
     int threshold; //may be above or below.
 };
 
@@ -88,18 +88,18 @@ struct Sensor groundSensorRight = { 2, 3650 };
 
 int untilSeesTapeLeft(void) // at least one
 {
-	return !isSensorAboveThreshold(groundSensorLeft);
+    return !isSensorAboveThreshold(groundSensorLeft);
 }
 
 int untilSeesTapeRight(void) // at least one
 {
-	return !isSensorAboveThreshold(groundSensorRight);
+    return !isSensorAboveThreshold(groundSensorRight);
 }
 
 int untilBothSeesTape(void)
 {
-	return !isSensorAboveThreshold(groundSensorLeft) ||
-           !isSensorAboveThreshold(groundSensorRight);
+    return !isSensorAboveThreshold(groundSensorLeft) ||
+        !isSensorAboveThreshold(groundSensorRight);
 }
 
 typedef int (*conditionFunction)(void);
@@ -110,7 +110,7 @@ typedef int (*conditionFunction)(void);
 
 void moveRobotVert(int dir) // -1,0,1 = back, stop, forward
 {
-	setMotorVelocity(motorFrontLeft, dir);
+    setMotorVelocity(motorFrontLeft, dir);
     setMotorVelocity(motorFrontRight, dir);
     setMotorVelocity(motorBackLeft, dir);
     setMotorVelocity(motorBackRight, dir);
@@ -118,7 +118,7 @@ void moveRobotVert(int dir) // -1,0,1 = back, stop, forward
 
 void moveRobotHoriz(int dir) // -1,0,1 = left, stop, right
 {
-	setMotorVelocity(motorFrontLeft, -dir);
+    setMotorVelocity(motorFrontLeft, -dir);
     setMotorVelocity(motorFrontRight, dir);
     setMotorVelocity(motorBackLeft, dir);
     setMotorVelocity(motorBackRight, -dir);
@@ -142,11 +142,11 @@ void pivotRobot(int deg, int time)
     if(deg==1)
     {
         setMotorVelocity(motorFrontLeft, deg);
-    	setMotorVelocity(motorBackLeft, deg);
+        setMotorVelocity(motorBackLeft, deg);
     } else
     {
         setMotorVelocity(motorFrontRight, -deg);
-    	setMotorVelocity(motorBackRight, -deg);
+        setMotorVelocity(motorBackRight, -deg);
     }
     msleep(time);
     setMotorVelocity(motorFrontLeft, 0);
@@ -159,10 +159,10 @@ void pivotRobot(int deg, int time)
 
 void rotateRobotColor(int deg, char sensor)
 {
-    
+
     if(sensor=='L')
     {
-        
+
         while (!isSensorAboveThreshold(groundSensorLeft))
         {
             setMotorVelocity(motorFrontLeft, deg);
@@ -184,47 +184,47 @@ void rotateRobotColor(int deg, char sensor)
 
 void moveVert(int dir, conditionFunction condition)
 {
-	while (condition()) {
-    	moveRobotVert(1*dir);
+    while (condition()) {
+        moveRobotVert(1*dir);
     }
     moveRobotVert(0);
 }
 
 void moveHoriz(int dir, conditionFunction condition)
 {
-	while (condition()) {
-    	moveRobotHoriz(1*-dir);
+    while (condition()) {
+        moveRobotHoriz(1*-dir);
     }
     moveRobotHoriz(0);
 }
 
 void moveTimeVert(int dir, int time)
 {
-	moveRobotVert(dir);
+    moveRobotVert(dir);
     msleep(time);
     moveRobotVert(0);
 }
 
 void moveTimeHoriz(int dir, int time)
 {
-	moveRobotHoriz(-dir);
+    moveRobotHoriz(-dir);
     msleep(time);
     moveRobotHoriz(0);
 }
 
 void moveCorrected(int dir, conditionFunction condition)
 {
-	while (condition()) 
+    while (condition()) 
     {
-    	moveRobotVert(1);
+        moveRobotVert(1);
         if (isSensorAboveThreshold(groundSensorLeft) && condition())
         {
-        	rotateRobot(-1, 100);
+            rotateRobot(-1, 100);
         }
-        
+
         else if (isSensorAboveThreshold(groundSensorRight) && condition()) 
-       	{
-        	rotateRobot(1, 100);
+        {
+            rotateRobot(1, 100);
         }
     }
     moveRobotVert(0);
@@ -232,16 +232,16 @@ void moveCorrected(int dir, conditionFunction condition)
 
 void moveCorrectedTime(int dir, int time)
 {
-	for(int i=0;i<=time;i++)
+    for(int i=0;i<=time;i++)
     {
-    	moveRobotVert(1);
+        moveRobotVert(1);
         if (isSensorAboveThreshold(groundSensorLeft))
         {
-        	rotateRobot(-1, 100);
+            rotateRobot(-1, 100);
         }
         else if (isSensorAboveThreshold(groundSensorRight)) 
-       	{
-        	rotateRobot(1, 100);
+        {
+            rotateRobot(1, 100);
         }
         msleep(1); //Changing this value doesnt effect speed unless >1
         printf("%5d",i);
@@ -265,7 +265,7 @@ int main()
 {
     enable_servos();
     //test individual functions easily (make sure to put break)
-    switch(2)
+    switch(6)
     {
         case 0: // Run this in the competition
             waitUntilLight();
@@ -279,11 +279,11 @@ int main()
             msleep(1000);
             setClawPosition(clawCloseVal);
             msleep(500);
-			setClawMoverPosition(clawMoverLeftUpVal, clawMoverRightUpVal);
+            setClawMoverPosition(clawMoverLeftUpVal, clawMoverRightUpVal);
             msleep(1000);
             setClawMoverPosition(clawMoverLeftDownVal, clawMoverRightDownVal);
-   			msleep(100);
-    		setPlowPosition(plowUpVal);
+            msleep(100);
+            setPlowPosition(plowUpVal);
             msleep(1000);
             run();
             break;
@@ -304,7 +304,7 @@ int main()
             msleep(500);
             setClawMoverPosition(1147, 900); // values to latch onto the right door
             msleep(500);
-            
+
             setClawPosition(clawHalfVal);
             msleep(500);
             setClawPosition(clawOpenVal);
@@ -321,40 +321,42 @@ int main()
             setPlowPosition(plowDownVal);
         default:
             break;
+        case 6:
+            run();
+            break;
     }
     return 0;
 }
 
 void run()
 {
-    
+
     // NOTE: functions now take a condition parameter so 
     // instead of "moveUntilSeesTape(1);"
     // it is "move(1, untilSeesTape);"
-    
-    
-    
- /* // Bring plow down and drive past both tapes, grabbing four poms before bringing the claw up and preparing for a turn
+
+  /*  
+    // Bring plow down and drive past both tapes, grabbing four poms before bringing the claw up and preparing for a turn
     setPlowPosition(plowDownVal);
     msleep(500);
-	moveVert(1, untilBothSeesTape);
+    moveVert(1, untilBothSeesTape);
     moveTimeVert(1, 1000);
     moveVert(1, untilBothSeesTape);
     setClawMoverPosition(clawMoverLeftUpVal, clawMoverRightUpVal);
     moveTimeVert(1, 450);
-    
-    
+
+
     // Turn and gather poms up to the first traffic cone, aligning to prepare to open doors
     rotateRobot(1, 1750);
     moveTimeVert(-1, 500);
-	moveCorrected(1, untilBothSeesTape);
+    moveCorrected(1, untilBothSeesTape);
     moveTimeVert(1, 2000);
     moveTimeVert(-1, 2000);
     rotateRobot(-1, 175);
     moveTimeHoriz(1, 2500);
     moveTimeHoriz(-1, 2250);
     moveTimeHoriz(1, 500);
-    
+
     // Open left door
     moveTimeHoriz(1, 500); // go back so bringing claw down does not hit the doors yet
     msleep(100);
@@ -365,7 +367,7 @@ void run()
     moveTimeHoriz(-1, 700); // move towards door
     moveTimeVert(-1, 2350); // we are latched on, just move left to move the door left
     moveTimeVert(1, 800); // unhook from the door 
-    
+
     // Open right door
     moveTimeVert(1, 500); // unhook from the left door
     moveTimeHoriz(1, 800); // move away from the doors
@@ -375,10 +377,11 @@ void run()
     moveTimeVert(1, 700);
     moveTimeHoriz(-1, 700); // hook onto door
     moveTimeVert(1, 2075); // move door right
-    
+
     // Prepare to grab Botguy
     moveTimeVert(-1, 400); // unhook from right door
     moveTimeHoriz(1, 700); // move back from right door so we do not get caught on it
+    setClawMoverPosition(clawMoverLeftDownVal, clawMoverRightDownVal);
     moveVert(-1, untilBothSeesTape); // go back to middle
     moveTimeVert(1, 650); // position back to middle
     setClawPosition(clawCloseVal); // close claw to enter the opening
@@ -391,12 +394,12 @@ void run()
     moveTimeHoriz(-1, 100); // get into botguy range
     setClawPosition(clawOpenVal); // open claw
     moveTimeHoriz(-1, 200); // get into botguy range
-    
+
     // Grab Botguy
     moveTimeHoriz(-1, 650); // fully enter botguy range
     msleep(500);
     setClawPosition(clawCloseVal); // grab botguy
-    
+
     // Get out of jail and go to second cone
     msleep(500);
     setClawMoverPosition(1697, 350);
@@ -406,39 +409,54 @@ void run()
     setClawMoverPosition(clawMoverLeftUpVal, clawMoverRightUpVal);
     moveCorrected(1, untilBothSeesTape);
     rotateRobot(-1, 175);
-    
+
     // Turn around and get the items into the lower starting box
-	rotateRobot(1, 3500);
-    moveCorrected(1, untilBothSeesTape);
+    rotateRobot(1, 3500);
+    moveCorrected(1, untilBothSeesTape); */
     moveTimeVert(1, 500);
-    moveCorrectedTime(1, 750); */
-    moveTimeVert(1, 1100); // PINK TAPE STARTS HERE, position for a turn
+    moveCorrectedTime(1, 750);
+    moveTimeVert(1, 2600); // PINK TAPE STARTS HERE, position for a turn
     pivotRobot(-1, 3750); // pivot 90 degrees
     moveTimeVert(1, 2000); // plow items just before the ramp
-    
-    // Pivot to plow everything up
+
+    // Pivot to get into the corner after pushing cubes away
     setPlowPosition(2047); // bring plow fully up
     moveTimeVert(-1, 1700); // back up
-	moveTimeHoriz(1, 800);
+    moveTimeHoriz(1, 1250); // go right to move cubes
     setPlowPosition(plowDownVal);
-	moveTimeVert(1, 1700);
-	moveTimeHoriz(-1, 800);
+    moveTimeVert(1, 1700); // go towards cubes
+    moveTimeHoriz(-1, 1100); // move cubes left
     setPlowPosition(2047); // bring plow fully up
-    moveTimeVert(-1, 1700);
+    moveTimeVert(-1, 1700); // back up
     moveTimeHoriz(1, 4250); // align with wall left of lower starting box
     moveTimeHoriz(-1, 750); // move a little off the wall
     rotateRobot(-1, 1750); // rotate 90 degrees
     moveTimeVert(-1, 1200); // align with wall behind robot
     moveTimeHoriz(1, 4750); // move right to get into position to plow
-    
-    // Knock down cubes and get ready to plow
+
+    // Get plow down
     moveTimeVert(-1, 500); // align with wall behind robot
-    moveTimeVert(1, 1500); // drive into cubes
-    moveTimeVert(-1, 1500); // go back
+    moveTimeVert(1, 2500); // drive into cubes
+    moveTimeVert(-1, 2500); // go back
     moveTimeHoriz(1, 400); // get into corner
     moveTimeVert(-1, 400); // get into corner
-    setPlowPosition(plowDownVal);
+    moveTimeHoriz(-1, 400); // go left a little to execute the turn
+    pivotRobot(1, 800); // turn right
+    setPlowPosition(plowDownVal); // drop the plow on the pvc
+    moveTimeVert(1, 200); // return back to the corner
+    moveTimeHoriz(1, 200); // return back to the corner
+    moveTimeVert(1, 200); // return back to the corner
+    moveTimeHoriz(1, 200); // return back to the corner
+    pivotRobot(-1, 400); // turn left to bring the plow down onto the playing field
+    msleep(200); 
+    setPlowPosition(100); // bring plow up a little before getting onto the ramp so it does not get caught on anything
+   
+    // Go up the ramp
+    moveVert(1, untilBothSeesTape); // go to the start of the ramp
+    setPlowPosition(plowDownVal); // plow goes down
+    moveTimeVert(1, 2000); // get off the tape
+    moveCorrectedTime(1, 6000); // go up the ramp
+    moveTimeVert(1, 2000); // get past top
+    moveCorrected(1, untilBothSeesTape); // go to the start of the upper starting box
     
-    // 
 }
-
