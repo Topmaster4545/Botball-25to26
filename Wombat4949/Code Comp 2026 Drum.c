1d75024c12814474671784aa8603532cb0a4f87a
Dylan H. <topmaster4545@gmail.com>
2026-04-29 22:32:27 -0400

    4/29/26 Code Changes
    -Pathfinding for drum robot should mostly be complete aside from pole alignment
    -Shake and dropoff should work but can be finicky
    -ALMOST COMPETITION TIME. FINAL DAYS OF LOCKING IN

--- Wombat4949/Code Comp 2026 Drum.c
+++ Wombat4949/Code Comp 2026 Drum.c
@@ -1,7 +1,7 @@
 #include <kipr/wombat.h>
 
 // DECLARATIONS
-int colorList[8];
+int colorList[8]={1,0,1,0,1,0,1,0};
 
 // MOTORS ---------------------------------------------------------
 
@@ -16,8 +16,8 @@ void setMotorVelocity(struct Motor m, int modifier)
     mav(m.port, m.velocity * modifier);
 }
 
-struct Motor motorFrontLeft  = { 1, 1000 };
-struct Motor motorFrontRight = { 0, 1300 };
+struct Motor motorFrontLeft  = { 1, 1250 };  //1150
+struct Motor motorFrontRight = { 0, 1400 };  //1400
 struct Motor motorRevolver   = { 1, -1250 };
 
 // SERVOS --------------------------------------------------------
@@ -27,7 +27,7 @@ struct Servo
 	int port;
 };
 
-void setServoPosition(struct Servo s, int pos) // 0/2047 = up/down
+void setServoPosition(struct Servo s, int pos) // 0/2047 = up/down   700 for door
 {
     set_servo_position(s.port, pos);
 } 
@@ -35,6 +35,7 @@ void setServoPosition(struct Servo s, int pos) // 0/2047 = up/down
 struct Servo servoArmRight = { 0 };
 struct Servo servoArmLeft = { 1 };
 struct Servo door = { 2 };
+struct Servo micro = {3};
 
 // SENSORS --------------------------------------------------------
 
@@ -49,8 +50,8 @@ int isSensorAboveThreshold(struct Sensor s)
 }
 
 struct Sensor lightSensor = { 3, 2800 };
-struct Sensor groundSensorLeft = { 2, 2500 };
-struct Sensor groundSensorRight = { 1, 2500 };
+struct Sensor groundSensorLeft = { 2, 3300 };
+struct Sensor groundSensorRight = { 1, 3300 };
 struct Sensor distanceSensorStart = { 0, 1250 };
 struct Sensor distanceSensorClose = { 0, 2750 };
 
@@ -105,7 +106,7 @@ void rotateRobot(int deg)
 // higher move funcctionss
 
 // basic move
-
+void moveTime(int dir, int time);
 void moveTime(int dir, int time)
 {
 	moveRobot(dir);
@@ -174,13 +175,8 @@ void moveCorrectedUntil(int dir, conditionFunction condition)
     moveRobot(0);
 }
 
+
 // SERVO
-
-void setDoorPosition(int pos)
-{
-	setServoPosition(door, pos);
-}
-
 void setArmPositionInc(int pos)
 {
     int currentPos=get_servo_position(0);
@@ -228,54 +224,9 @@ void incrementDebug()
     debug++;
 }
 
-int main()
-{
-    enable_servos();
-    setDoorPosition(2047);
-    /*mtp(2,100,0);
-    setArmPositionInc(50);
-    msleep(5000); *///DELETE OR LESSEN TIME BEFORE COMP
-    //test individual functions easily (make sure to put break)
-    switch(1)
-    {
-        case 0: // Run this in the competition
-            waitUntilLight();
-            shut_down_in(120);
-            run();
-            break;
-        case 1: // do NOT run this in the competition
-            run();
-            break;
-        default:
-            break;
-    }
-    return 0;
-}
-
-void run()
-{   
-    /*moveUntil(1,seesWallStart);
-    rotateTime(-1,1175);
-	moveUntil(1, bothSeesTape);
-    moveUntil(1, bothSeesTape);
-    moveUntil(1, seesWallClose);
-    setArmPositionInc(2047);*/
-    
-    //Moves out from starting box to the drums
-    camera_open();
-    /*printf("starting\n");
-    double startTime=seconds();
-    setArmPositionInc(950);
-    moveUntil(1, bothSeesTape);
-    moveTime(1,2750);
-    setArmPositionInc(2047);
-    moveTime(-1,250);
-    rotateTime(-1,250);
-    printf("before delay with %f\n",seconds()-startTime);*/
-    
-    //Collects Drums
-
-    camera_update();
+//DRUMS
+void drumCollectionSequence(){
+	camera_update();
     newImage();
     for(int i=1; i<=8; i++)
     {
@@ -293,7 +244,7 @@ void run()
         }
         printf("pipe detected! ");
         msleep(600);
-        setDoorPosition(500);
+        setServoPosition(door,500);
         mrp(2,75,225);
         msleep(2000);
         for(int i=0;i<8;i++)
@@ -301,11 +252,232 @@ void run()
             printf("%d,",colorList[i]);
         }
         printf("\n");
-        setDoorPosition(2047);
+        setServoPosition(door,2047);
         //msleep(5000);
         newImage();
         
     }
     printf("ended\n");
+}
+void shake(){
+	int orig = gmpc(2);
+    mav(2,-355);
+    msleep(340);
+    
+   	mav(2,0);
+    for(int i=1400;i>=1200;i-=50)
+    {
+        mav(2,0);
+        mav(2,-i);
+        msleep(200);
+        mav(2,0);
+        msleep(500);
+        mav(2,i);
+        msleep(200);
+        mav(2,0);
+        
+    }
+    
+    mav(2,-275);
+    msleep(500);
+    mav(2, 200);
+    msleep(1000);
+    mav(2,-275);
+    msleep(200);
+    mtp(2,200,orig);
+    msleep(2000);
+    
+}
+void dropoffSequence()
+{
+	int firstColor = colorList[5];
+    shake();
+    colorList[5]=-1;
+    int pCount = 0;
+    int temp;
+    while(pCount<3)
+    {
+        while(colorList[5]!=firstColor)
+        {
+            //cw turn
+            mav(2,450);
+            msleep(600);
+            mav(2,0);
+            msleep(300);
+            temp = colorList[0];
+            for(int j=0; j<=6; j++)
+            {
+                colorList[j]=colorList[j+1];
+            }
+            colorList[7] = temp;
+        }
+        shake();
+        msleep(400);
+        pCount++;
+        printf("pcountinc");
+        colorList[5]=-1;
+        for(int i=0;i<8;i++)
+        {
+            printf("%d,",colorList[i]);
+        }
+    }
+    if(firstColor==0)
+    {
+        while(colorList[5]!=1)
+        {
+            //cw turn
+            mav(2,450);
+            msleep(600);
+            mav(2,0);
+            msleep(300);
+            temp = colorList[0];
+            for(int j=0; j<=6; j++)
+            {
+                colorList[j]=colorList[j+1];
+            }
+            colorList[7] = temp;
+        } 
+    }else
+    {
+        while(colorList[5]!=0)
+        {
+            //cw turn
+            mav(2,450);
+            msleep(600);
+            mav(2,0);
+            msleep(300);
+            temp = colorList[0];
+            for(int j=0; j<=6; j++)
+            {
+                colorList[j]=colorList[j+1];
+            }
+            colorList[7] = temp;
+        }
+    }
+    printf("\nDROP OFF SEQUENCE DONE");
+}
 
+
+
+int main()
+{
+    enable_servos();
+    setServoPosition(door,400);
+    setServoPosition(micro,4000);
+    /*
+    mtp(2,100,0);
+    setArmPositionInc(1000);*/
+    //msleep(5000); ///DELETE OR LESSEN TIME BEFORE COMP
+    //test individual functions easily (make sure to put break)
+    switch(1)
+    {
+        case 0: // Run this in the competition
+            waitUntilLight();
+            shut_down_in(120);
+            run();
+            break;
+        case 1: // do NOT run this in the competition
+            run();
+            break;
+        case 2:
+			dropoffSequence();
+            break;
+            
+        case 3:
+            shake();
+          	break;
+        case 4:
+            moveCorrectedUntil(1,bothSeesTape);
+            moveTime(1,1000);
+            moveCorrectedUntil(1,bothSeesTape);
+            moveCorrectedTime(1,1500);
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+void run()
+{   
+    //Moves out from starting box to the drums
+    camera_open();
+    printf("starting\n");
+    double startTime=seconds();
+    setArmPositionInc(950);
+    msleep(1200);
+    moveUntil(1, bothSeesTape);
+    moveTime(1,1000);
+    moveUntil(1, bothSeesTape);
+    moveTime(1,2000);
+    setArmPositionInc(2047);
+    moveTime(-1,250);
+    rotateTime(-1,250);
+    printf("before delay with %f\n",seconds()-startTime);
+    
+    
+    //Collects Drums
+    //drumCollectionSequence();
+    setArmPositionInc(1000);
+    
+     //Work in prog, to move from tape, back to first pole
+    
+    moveUntil(-1,bothSeesTape);
+    msleep(300);
+    
+    rotateTime(-1,2250);
+    setServoPosition(micro,1400);
+    msleep(200);
+    moveTime(1,5750);
+    rotateTime(-1,150);
+    
+   	dropoffSequence();
+    
+    moveTime(-1,2000);
+    moveUntil(-1,seesTapeRight);
+    moveTime(-1,1500);
+    moveUntil(-1,seesTapeRight);
+    moveTime(-1,1500);
+    msleep(300);
+    rotateTime(-1,1000);
+    rotateUntil(-1,seesTapeRight);
+    moveCorrectedUntil(1,bothSeesTape);
+    moveTime(1,1000);
+    moveCorrectedUntil(1,bothSeesTape);
+    moveTime(1,1000);
+    moveCorrectedTime(1,1000);
+    rotateTime(1,150);
+    
+    dropoffSequence();
+   
+    /*moveTime(-1,4000);
+    msleep(400);
+    mav(0,-1830);
+    mav(1,-1000);
+    msleep(1500);
+    moveRobot(0);
+    rotateTime(-1,5000);
+    moveRobot(0);*/
+    
+  
+    /*
+    //ORIGINAL SEQUENCE AFTER BACKING UP FROM DISPENSER
+    moveTime(-1,1000);
+    rotateTime(1,3350);
+    moveTime(1,7000);
+    dropoffSequence();
+    moveUntil(-1,bothSeesTape);
+    moveTime(-1,500);
+    moveUntil(-1,bothSeesTape);
+    rotateUntil(-1,seesTapeLeft);
+    rotateTime(-1,450);
+    moveCorrectedUntil(1,bothSeesTape);
+    moveTime(1,600);
+    moveCorrectedUntil(1,bothSeesTape);
+    moveTime(1,500);
+    moveCorrectedTime(1,800);
+    shake();
+    */
+    
+    
 }
